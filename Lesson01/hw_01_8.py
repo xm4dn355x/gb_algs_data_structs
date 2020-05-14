18c2fdb09d50a5f8e09f6254f8b6b20bc7e9f3cf
Michael Nikitenko <xm4dn355x@gmail.com>
2020-05-14 15:23:04 +0500

    deleted crap

--- Lesson01/hw_01_8.py
+++ Lesson01/hw_01_8.py
@@ -34,25 +34,24 @@ def input_numbers():
             try:
                 res.append(float(nbr))
             except ValueError:
-                return res, False
-    return res, True
+                return False
+    return res
 
 
-def middle_number(numbers: list, type_check_flag: bool):
+def middle_number(numbers):
     """
     Получает список чисел, флаг правильности введенных значений и возвращает медианное значение из списка чисел.
 
     :param numbers: list of numbers
     :param type_check_flag: bool
     """
-    if type_check_flag == True:
-        print(f'Медианное значение равно: {statistics.median(numbers)}')
-    else:
+    if numbers == False:
         print('\nВы должны были ввести целое число, либо десятичную дробь.\nВ качестве разделителя десятичной дроби '
               'используется точка, например 5.0')
+    else:
+        print(f'Медианное значение равно: {statistics.median(numbers)}')
 
 
 if __name__ == '__main__':
     print('Lesson 1 task 8')
-    numbers, type_check_flag = input_numbers()
-    middle_number(numbers, type_check_flag)
+    middle_number(input_numbers())
